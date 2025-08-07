eac16b0304ca04ab5ea4d3bb1c9e87a8dfd9438b
JUNU555tn <105146928+JUNU555tn@users.noreply.github.com>
2025-08-07 16:57:00 +0600

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,7 +4,7 @@ class Config:
     API_ID = int(getenv("API_ID", "1990"))
     API_HASH = getenv("API_HASH", "6a6dcb56edce33056d37baca")
     BOT_TOKEN = getenv("BOT_TOKEN", "685b0jxs91L9_uKUQ0PG1lIveRAtd4vnSw")
-    FSUB = getenv("FSUB", "ROCKERSBACKUP")
+    FSUB = getenv("FSUB", "JNKBACKUP")
     CHID = int(getenv("CHID", "-100293873"))
     SUDO = int(getenv("SUDO", "6347574"))
     MONGO_URI = getenv("MONGO_URI", "mongodb@cluster0.8pzxa6s.mongodb.net/?retryWrites=true&w=majority")
